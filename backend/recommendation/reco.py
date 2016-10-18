acf7df45c795318f028954af0c986333b3df31f6
Betina Costa <betina.costa@sap.com>
2016-10-18 15:49:36 -0200

    Get user with at leats one genre in common

--- backend/recommendation/reco.py
+++ backend/recommendation/reco.py
@@ -1,4 +1,4 @@
-from recommendation.models import Movie
+from recommendation.models import Movie, User
 
 def teste():
     query = Movie.objects.all()
@@ -7,3 +7,5 @@ def teste():
 def getFacebookFriends(user_id):
     pass
 
+def getSimilarProfiles(user_id):
+    oneGenreSimilarity = User.objects.raw("SELECT distinct user.user_ID FROM user INNER JOIN profile ON profile.user_id = user.user_id INNER JOIN profile_genre ON profile.profile_id = profile_genre.profile_id AND (profile_genre.genre_id = '1' OR profile_genre.genre_id = '5' OR profile_genre.genre_id = '13')")
