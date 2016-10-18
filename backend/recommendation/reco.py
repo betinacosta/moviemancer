c29720b99496c4b6b72501aa52879ae18adb02b3
Betina Costa <betina.costa@sap.com>
2016-10-18 16:53:53 -0200

    get users with two or more genres in common

--- backend/recommendation/reco.py
+++ backend/recommendation/reco.py
@@ -1,4 +1,6 @@
 from recommendation.models import Movie, User
+import collections
+
 
 def teste():
     query = Movie.objects.all()
@@ -8,6 +10,18 @@ def getFacebookFriends(user_id):
     pass
 
 def getSimilarProfiles(user_id):
-    oneGenreSimilarity = User.objects.raw("SELECT distinct user.user_ID FROM user INNER JOIN profile ON profile.user_id = user.user_id INNER JOIN profile_genre ON profile.profile_id = profile_genre.profile_id AND (profile_genre.genre_id = '1' OR profile_genre.genre_id = '5' OR profile_genre.genre_id = '13')")
+    oneGenreSimilarity = User.objects.raw("SELECT user.user_ID FROM user INNER JOIN profile ON profile.user_id = user.user_id INNER JOIN profile_genre ON profile.profile_id = profile_genre.profile_id AND (profile_genre.genre_id = '1' OR profile_genre.genre_id = '5' OR profile_genre.genre_id = '13')")
 
+    usersList = []
+    similarUsers = []
+
+    for user in oneGenreSimilarity:
+        usersList.append(user.user_id)
+
+    for item, count in collections.Counter(usersList).items():
+        if count > 1:
+            similarUsers.append(item)
+
+    
+  
     return oneGenreSimilarity
