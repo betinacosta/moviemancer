e4059ac088af6ba669319d2f27cbc7243f969be7
Betina Costa <bmcosta13@gmail.com>
2016-10-23 01:21:30 -0200

    create query file

---
+++ backend/recommendation/queries.py
@@ -0,0 +1,11 @@
+from recommendation.models import Movie, User, List, Rating, Rate, MovieList,
+
+def movie_by_user_list(user_id, list_name):
+    
+    for item in List.objects.raw("SELECT * FROM type WHERE type_name = %s", [list_name])
+        type_id = type_id;
+
+    for item in List.objects.raw("SELECT list_ID FROM list WHERE user_id = %s AND type_id = %s", [user_id, type_id]):
+        list_id = item.list_id
+
+    return item in Movie.objects.raw("SELECT * from movie INNER JOIN movie_list ON list_id = %s AND movie_list.movie_id = movie.movie_id", [list_id])
