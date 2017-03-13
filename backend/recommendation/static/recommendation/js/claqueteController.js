f1e65a8e5f47b44bacdc9714b7a7602d14fe9896
Costa <betina.costa@sap.com>
2017-03-13 15:43:58 -0300

    add star rating directive

--- backend/recommendation/static/recommendation/js/claqueteController.js
+++ backend/recommendation/static/recommendation/js/claqueteController.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', []).config(function($interpolateProvider) {
+var app = angular.module('myApp', ['ngRateIt']).config(function($interpolateProvider) {
         $interpolateProvider.startSymbol('{$');
         $interpolateProvider.endSymbol('$}');
     });
