b1fbcfd69cf1416282c2280770a40283472573f1
Costa <betina.costa@sap.com>
2017-03-21 16:59:35 -0300

    backend of movie details

--- backend/recommendation/static/recommendation/js/movieDetailController.js
+++ backend/recommendation/static/recommendation/js/movieDetailController.js
@@ -4,8 +4,208 @@ var app = angular.module('myApp3', ['ngRateIt']).config(function($interpolatePro
     });
 
 app.controller('movieCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
-    console.log('vamo dale, dale vamo');
-    console.log($routeParams.tmdbID);
-    $scope.batata = 'batata';
+
+     
+    		//tmdb wrapper
+		(function () {
+			window.tmdb = {
+				"api_key": "5880f597a9fab4f284178ffe0e1f0dba",
+				"base_uri": "http://api.themoviedb.org/3",
+				"images_uri": "http://image.tmdb.org/t/p",
+				"timeout": 5000,
+				call: function (url, params, success, error) {
+					var params_str = "api_key=" + tmdb.api_key;
+					for (var key in params) {
+						if (params.hasOwnProperty(key)) {
+							params_str += "&" + key + "=" + encodeURIComponent(params[key]);
+						}
+					}
+					var xhr = new XMLHttpRequest();
+					xhr.timeout = tmdb.timeout;
+					xhr.ontimeout = function () {
+						throw ("Request timed out: " + url + " " + params_str);
+					};
+					xhr.open("GET", tmdb.base_uri + url + "?" + params_str, true);
+					xhr.setRequestHeader('Accept', 'application/json');
+					xhr.responseType = "text";
+					xhr.onreadystatechange = function () {
+						if (this.readyState === 4) {
+							if (this.status === 200) {
+								if (typeof success == "function") {
+									success(JSON.parse(this.response));
+								} else {
+									throw ('No success callback, but the request gave results')
+								}
+							} else {
+								if (typeof error == "function") {
+									error(JSON.parse(this.response));
+								} else {
+									throw ('No error callback')
+								}
+							}
+						}
+					};
+					xhr.send();
+				}
+			}
+		})()
+	//tmdb wrapper
+
+    $scope.loadMovieDetails = function () {
+			oParams = {
+				"language": "pt-BR"
+			};
+
+			$scope.movieDetails = {};
+            $scope.movieGenres = [];
+			$scope.imagePath = 'https://image.tmdb.org/t/p/original/';
+
+			tmdb.call("/movie/" + $routeParams.tmdbID, oParams,
+				function (details, movieDetails, imagePath, movieGenres) {
+					$scope.movieDetails = {
+                            title: details.title,
+                            poster_path: 'https://image.tmdb.org/t/p/original/' + details.poster_path,
+                            original_title: details.original_title,
+                            overview: details.overview,
+                            year: details.release_date,
+                            runtime: details.runtime,
+                            imdb_link: 'http://www.imdb.com/title/' + details.imdb_id
+						}
+
+                    for (i = 0; i < details.genres.length; i++) {
+                        $scope.movieGenres.push(details.genres[i].name);
+                    }
+				},
+				function (e) {
+					console.log("Error: " + e)
+				}
+			);
+		};
+
+    $scope.loadMovieVideo = function () {
+			
+			$scope.movieVideo = {};
+            oParams = {};
+
+			tmdb.call("/movie/" + $routeParams.tmdbID + '/videos', oParams,
+				function (video, movieVideo) {
+					$scope.movieVideo = {
+                            video_title: video.results[0].name,
+                            youtube_link: 'https://www.youtube.com/watch?v=' + video.results[0].key,
+                    }
+				},
+				function (e) {
+					console.log("Error: " + e)
+				}
+			);
+		};
+
+    $scope.loadCast = function () {
+			
+			$scope.movieCast = [];
+            oParams = {};
+
+			tmdb.call("/movie/" + $routeParams.tmdbID + '/credits', oParams,
+				function (casting, movieCast) {
+
+                    for(i=0; i < 8; i++) {
+                        $scope.movieCast.push({
+                            character: casting.cast[i].character,
+                            actor: casting.cast[i].name,
+                            profile_img: 'https://image.tmdb.org/t/p/original/' + casting.cast[i].profile_path
+                        });
+                    }
+				},
+				function (e) {
+					console.log("Error: " + e)
+				}
+			);
+		};
+
+    $scope.loadCrew = function () {
+			
+			$scope.movieCrew = {};
+            oParams = {};
+
+			tmdb.call("/movie/" + $routeParams.tmdbID + '/credits', oParams,
+				function (crew, movieCrew) {
+                    director = '';
+                    writer = '';
+
+                    for(i=0; i < crew.crew.length; i++) {
+
+                        if (crew.crew[i].job == "Director") {
+                            director = director + ', ' + crew.crew[i].name
+                        }
+                        if (crew.crew[i].job == "Writer") {
+                            writer = writer + ', ' + crew.crew[i].name
+                        }
+                    }
+
+                    movieCrew = {
+                        director: director,
+                        writer: writer
+                    }
+				},
+				function (e) {
+					console.log("Error: " + e)
+				}
+			);
+		};
+
+    $scope.loadKeywords = function () {
+			
+			$scope.movieKeywords = [];
+            oParams = {};
+
+			tmdb.call("/movie/" + $routeParams.tmdbID + '/keywords', oParams,
+				function (keywords, movieKeywords) {
+
+                    for(i=0; i < 5; i++) {
+                        $scope.movieKeywords.push(keywords.keywords[i].name);
+                    }
+				},
+				function (e) {
+					console.log("Error: " + e)
+				}
+			);
+		};
+
+     $scope.loadSimilarMovies = function () {
+			
+			$scope.similarMovies = [];
+            oParams = {
+				"language": "pt-BR"
+			};
+
+			tmdb.call("/movie/" + $routeParams.tmdbID + '/similar', oParams,
+				function (similar, similarMovies) {
+
+                    for(i=0; i < 6; i++) {
+                        $scope.similarMovies.push({
+                            title: similar.results[i].title,
+                            poster_path: 'https://image.tmdb.org/t/p/original/' + similar.results[i].poster_path,
+                            tmdb_id: similar.results[i].id
+                        });
+                    }
+				},
+				function (e) {
+					console.log("Error: " + e)
+				}
+			);
+		};
+
+    $scope.init = function () {
+        $scope.loadMovieDetails();
+        $scope.loadMovieVideo();
+        $scope.loadCast();
+        $scope.loadCrew();
+        $scope.loadKeywords();
+        $scope.loadSimilarMovies();
+    }
+
+    $scope.init();
+
+    
 
 }]);
