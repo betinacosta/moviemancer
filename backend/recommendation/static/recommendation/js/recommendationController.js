383b9221f756b24fb1add7cafda73b785909dfed
Betina Costa <bmcosta13@gmail.com>
2017-04-21 02:53:01 -0300

    add rate and watchlist

--- backend/recommendation/static/recommendation/js/recommendationController.js
+++ backend/recommendation/static/recommendation/js/recommendationController.js
@@ -17,10 +17,13 @@ $scope.getRecommendation = function (movies) {
 	$http.get('reco').success(function(data) {
 
 		for (i = 0; i < data.length; i++) {
-			$scope.fullRecommendation.push ({title: data[i].tmdb_title, poster: data[i].tmdb_poster});
+			$scope.fullRecommendation.push ({   title: data[i].tmdb_title, 
+                                                poster: data[i].tmdb_poster, 
+                                                movie_id: data[i].movie_id, 
+                                                tmdb_id: data[i].tmdb_movie_id});
 		}
 
-        $scope.teste = $scope.splitRecommendations($scope.fullRecommendation, Math.ceil($scope.fullRecommendation.length/6), false);
+        $scope.fullList = $scope.splitRecommendations($scope.fullRecommendation, Math.ceil($scope.fullRecommendation.length/6), false);
         for (i = 0; i < $scope.fullRecommendation.length/6; i++) {
             $scope.index.push(i);
         }
@@ -88,5 +91,60 @@ $scope.showFilterBar = function() {
 },
 // Call init function
 	$scope.init();
+
+$scope.setUserRating = function (rating, movieID) {
+
+		$http.post("ratemovie/", {
+				"movie_id": movieID,
+				"rate_id": rating,
+				"user_id": 1
+			}, {
+				'Content-Type': 'application/json; charset=utf-8'
+			})
+			.then(
+				function (response) {
+					$scope.getRecommendation();
+					console.log('Success: ', response.data)
+					$scope.toastMessege("Filme Adicionado a Lista de Vistos")
+				},
+				function (response) {
+					console.log('Error: ', response)
+				}
+			);
+
+			
+	}
+
+	$scope.addWatchlist = function (movieID) {
+
+		$http.post("addwatchlist/", {
+				"movie_id": movieID,
+				"user_id": 1
+			}, {
+				'Content-Type': 'application/json; charset=utf-8'
+			})
+			.then(
+				function (response) {
+					console.log('Success: ', response.data)
+					$scope.getRecommendation();
+					$scope.toastMessege("Filme Adicionado a Quero Ver")
+				},
+				function (response) {
+					console.log('Error: ', response)
+				}
+			);
+	}
+
+$scope.toastMessege = function (msg) {
+		$scope.toastMessage = msg;
+		// Get the snackbar DIV
+		var x = document.getElementById("snackbar")
+
+		// Add the "show" class to DIV
+		x.className = "show";
+
+		// After 3 seconds, remove the show class from DIV
+		setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
 
 }]);
