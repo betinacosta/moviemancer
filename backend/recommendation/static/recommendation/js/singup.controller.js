bb93d30dd76f012a8581da3eb3a567c0f278aad3
Betina Costa <betina.costa@sap.com>
2017-04-26 11:26:27 -0300

    fix footer issue

--- backend/recommendation/static/recommendation/js/singup.controller.js
+++ backend/recommendation/static/recommendation/js/singup.controller.js
@@ -4,5 +4,10 @@ var app = angular.module('singupApp', []).config(function ($interpolateProvider)
 });
 
 app.controller('singupCtrl', ['$scope', function ($scope) {
-
+	$scope.footerID = document.getElementById('footer');
+    $scope.bodyID = document.getElementById('body');
+    $scope.footerID.style.position = 'absolute';
+    $scope.footerID.style.bottom = '0';
+    $scope.footerID.style.width = '100%';
+    $scope.bodyID.style.backgroundColor = '#eee';
 }]);
