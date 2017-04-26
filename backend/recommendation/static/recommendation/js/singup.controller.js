2fe5c42d25aaae9f76775a0651e5885041039655
Betina Costa <betina.costa@sap.com>
2017-04-26 14:43:45 -0300

    add filed validation

--- backend/recommendation/static/recommendation/js/singup.controller.js
+++ backend/recommendation/static/recommendation/js/singup.controller.js
@@ -3,11 +3,60 @@ var app = angular.module('singupApp', []).config(function ($interpolateProvider)
 	$interpolateProvider.endSymbol('$}');
 });
 
-app.controller('singupCtrl', ['$scope', function ($scope) {
+app.controller('singupCtrl', ['$scope', '$window','$rootScope', function ($scope, $window, $rootScope) {
 	$scope.footerID = document.getElementById('footer');
     $scope.bodyID = document.getElementById('body');
     $scope.footerID.style.position = 'absolute';
     $scope.footerID.style.bottom = '0';
     $scope.footerID.style.width = '100%';
     $scope.bodyID.style.backgroundColor = '#eee';
+
+    $scope.validateEntry = function(name, email, password, confirmedPassword) {
+        if(name == null || email == null || password == null || confirmedPassword == null) {
+            if(name == null) {
+                $scope.invalidateField('name');
+            }
+
+            if(email == null) {
+                $scope.invalidateField('email');
+            }
+
+            if(password == null) {
+                $scope.invalidateField('password');
+            }
+
+            if(confirmedPassword == null) {
+                 $scope.invalidateField('passwordConfirm');
+            }
+        }else {
+            if(password == confirmedPassword) {
+                $scope.chooseGenres(name, email, password, confirmedPassword);
+            }else {
+                $scope.invalidateField('passwordConfirm');
+                $scope.invalidateField('password');
+            }
+            
+        }
+    }
+
+    $scope.chooseGenres = function(name, email, password, confirmedPassword) {
+
+        $rootScope.registration = {name: name, email: email, password: password, confirmedPassword: confirmedPassword}
+        console.log($rootScope.registration);
+        console.log('Ops')
+
+        //$window.location('/registration')
+    }
+
+    $scope.clearField = function(fieldID){
+        $scope.nameID = document.getElementById(fieldID);
+        $scope.nameID.style.borderColor  = '#7d7b7b';
+    }
+
+    $scope.invalidateField = function(fieldID){
+        $scope.nameID = document.getElementById(fieldID);
+        $scope.nameID.style.borderColor  = 'red';
+    }
 }]);
+
+
